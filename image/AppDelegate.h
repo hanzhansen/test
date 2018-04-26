923d8549026302be2617c526bc641f05227a4ca7
韩占森 <13663951347@163.com>
2018-04-26 15:19:51 +0800

    Initial Commit

---
+++ image/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  image
+//
+//  Created by 韩占森 on 2018/4/26.
+//  Copyright © 2018年 韩占森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ image/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  image
+//
+//  Created by 韩占森 on 2018/4/26.
+//  Copyright © 2018年 韩占森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
